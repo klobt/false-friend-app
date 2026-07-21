530d752eb450e62e0d4372ab0b55b5a67234b449
mpus <mateuszpodmokly@outlook.com>
2026-07-21 22:19:17 +0200

    try catch dla pobierania danych z API

--- android-app/app/src/main/java/org/agh/falsefriendapp/data/repository/ExerciseRepository.kt
+++ android-app/app/src/main/java/org/agh/falsefriendapp/data/repository/ExerciseRepository.kt
@@ -1,22 +1,29 @@
 package org.agh.falsefriendapp.data.repository
 
+import android.util.Log
 import org.agh.falsefriendapp.data.api.RetrofitClient
 import org.agh.falsefriendapp.data.model.TranslationExercise
 
+private const val TAG = "ExerciseRepository"
+
 class ExerciseRepository {
     suspend fun getTranslationExercises(): List<TranslationExercise> {
-        // TODO try catch
-
         val ids = listOf(1, 2, 3, 4, 5)
-        val response = RetrofitClient.api.getExercises(ids)
 
-        return response.data.map { dto ->
-            TranslationExercise(
-                id = dto.id,
-                sentence = dto.content.word,
-                options = dto.content.answers,
-                correctAnswerIndex = dto.content.correctIdx
-            )
+        try {
+            val response = RetrofitClient.api.getExercises(ids)
+            return response.data.map { dto ->
+                TranslationExercise(
+                    id = dto.id,
+                    sentence = dto.content.word,
+                    options = dto.content.answers,
+                    correctAnswerIndex = dto.content.correctIdx
+                )
+            }
+        } catch (e: Exception) {
+            val msg = "Network error"
+            Log.e(TAG, msg, e)
+            return emptyList()
         }
     }
 
