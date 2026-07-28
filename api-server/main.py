44553d0f73e89d0030c9eb2d761c4f511f859308
klobt <111685189+klobt@users.noreply.github.com>
2026-07-28 16:49:21 +0200

    set avatar

--- api-server/main.py
+++ api-server/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Query
-from dao import ExerciseDao, SessionDao
-from model import Session
+from dao import ExerciseDao, SessionDao, UserDao
+from model import PublicUserData, Session
 
 app = FastAPI()
 
@@ -25,6 +25,27 @@ async def get_sessions(limit: int = Query(default=10), offset: int = Query(defau
 @app.post("/sessions/")
 async def post_session(session: Session):
     SessionDao().create(session)
+    return {
+        "success": True
+    }
+
+@app.get("/users/")
+async def get_users(limit: int = Query(default=10), offset: int = Query(default=0)):
+    return {
+        "data": UserDao().get(limit, offset),
+        "total": UserDao().total()
+    }
+
+@app.get("/users/{user_id}/")
+async def get_user(user_id: int):
+    return {
+        "id": user_id,
+        "public_data": UserDao().get_public_data(user_id)
+    }
+
+@app.put("/users/{user_id}/")
+async def put_user(user_id: int, user: PublicUserData):
+    UserDao().set_public_data(user.data, user_id)
     return {
         "success": True
     }
