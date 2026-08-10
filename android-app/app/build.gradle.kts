3ea10037b4cefbd0e36e7c3ba4f096b1ad26f875
mpus <mateuszpodmokly@outlook.com>
2026-08-10 19:14:17 +0200

    v0.4.1 (4)

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         minSdk = 26
         targetSdk = 37
 
-        versionCode = 3
-        versionName = "0.4.0"
+        versionCode = 4
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
