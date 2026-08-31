511d2a8ce879de25fd44257f60d8573fd5b8838c
klobt <111685189+klobt@users.noreply.github.com>
2026-09-01 00:07:50 +0200

    more exercises

---
+++ api-server/migrations/0007_more_exercises/up.sql
@@ -0,0 +1,544 @@
+INSERT INTO "exercises"
+    ("gen_id", "type", "data")
+VALUES
+    (
+        3,
+        101,
+        '{
+            "word": "actual",
+            "answers": ["rzeczywisty", "aktualny", "aktywny", "dokładny"],
+            "correct_idx": 0
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "sympathetic",
+            "answers": ["sympatyczny", "apatyczny", "współczujący", "patetyczny"],
+            "correct_idx": 2
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "fabric",
+            "answers": ["fabryka", "tkanina", "budowa", "struktura"],
+            "correct_idx": 1
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "ordinary",
+            "answers": ["ordynarny", "oficjalny", "zwykły", "szlachetny"],
+            "correct_idx": 2
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "eventual",
+            "answers": ["ewentualny", "wydarzeniowy", "ostateczny", "codzienny"],
+            "correct_idx": 2
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "preservative",
+            "answers": ["prezerwatywa", "konserwa", "konserwant", "lekarstwo"],
+            "correct_idx": 2
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "receipt",
+            "answers": ["recepta", "przepis", "paragon", "dowód"],
+            "correct_idx": 2
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "brave",
+            "answers": ["brawo", "odważny", "dzielny", "bojaźliwy"],
+            "correct_idx": 1
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "novel",
+            "answers": ["nowela", "opowiadanie", "powieść", "nowość"],
+            "correct_idx": 2
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "pension",
+            "answers": ["pensja", "wynagrodzenie", "emerytura", "odprawa"],
+            "correct_idx": 2
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "extra",
+            "answers": ["dodatkowy", "ekstra", "zewnętrzny", "nadzwyczajny"],
+            "correct_idx": 0
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "pasta",
+            "answers": ["pasta", "ciasto", "makaron", "mąka"],
+            "correct_idx": 2
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "magazine",
+            "answers": ["magazyn", "czasopismo", "gazeta", "książka"],
+            "correct_idx": 1
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "eventually",
+            "answers": ["w końcu", "ewentualnie", "możliwie", "przypadkowo"],
+            "correct_idx": 0
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "mansion",
+            "answers": ["mansarda", "rezydencja", "dom", "willa"],
+            "correct_idx": 1
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "band",
+            "answers": ["zespół", "banda", "taśma", "grupa"],
+            "correct_idx": 0
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "medicine",
+            "answers": ["lekarstwo", "medycyna", "zdrowie", "terapia"],
+            "correct_idx": 0
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "apartment",
+            "answers": ["mieszkanie", "apartament", "pokój", "dom"],
+            "correct_idx": 0
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "vacancy",
+            "answers": ["wakat", "wakacje", "urlop", "przerwa"],
+            "correct_idx": 0
+        }'
+    ),
+    (
+        3,
+        101,
+        '{
+            "word": "chef",
+            "answers": ["kucharz", "szef", "kelner", "gospodarz"],
+            "correct_idx": 0
+        }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "camera",
+              "answers": [
+                    "a device used for taking photographs",
+                    "a device used for recording moving images",
+                    "a small room used for private interviews"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "conductor",
+              "answers": [
+                    "a person who directs the performance of an orchestra",
+                    "a person who checks tickets on a train or bus",
+                    "a material that allows heat or electricity to pass through it"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "constipation",
+              "answers": [
+                    "a condition that causes difficulty in emptying the bowels",
+                    "a feeling of anxiety and confusion",
+                    "a state of being unable to move or act"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "apology",
+              "answers": [
+                    "a regretful acknowledgement of an offence or failure",
+                    "a formal written defence of a cause or theory",
+                    "a detailed explanation of a historical event"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "athlete",
+              "answers": [
+                    "a person who is proficient in sports and physical exercise",
+                    "a person who does not believe in the existence of God",
+                    "a person who studies the human body and movement"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "blank",
+              "answers": [
+                    "not filled or written on; empty",
+                    "a printed document with spaces for information to be inserted",
+                    "a short period of silence in a broadcast"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "clock",
+              "answers": [
+                    "an instrument for measuring and indicating time",
+                    "a small block used as a toy or building material",
+                    "a device that rings to wake someone up"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "biscuit",
+              "answers": [
+                    "a small baked unleavened cake, typically crisp and sweet",
+                    "a light sponge cake made with eggs and flour",
+                    "a thin slice of potato fried until crisp"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "cabinet",
+              "answers": [
+                    "a cupboard with shelves or drawers for storing articles",
+                    "a small room or compartment in a vehicle or aircraft",
+                    "a group of senior ministers or officials"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "comfortable",
+              "answers": [
+                    "providing physical ease and relaxation",
+                    "luxurious and of high quality",
+                    "suitable for a particular situation or purpose"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "concrete",
+              "answers": [
+                    "a building material made from cement, sand, and water",
+                    "specific and definite rather than vague or general",
+                    "related to a formal agreement between parties"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "condition",
+              "answers": [
+                    "the state of something with regard to its appearance or quality",
+                    "physical fitness and good health",
+                    "a legal requirement or stipulation in a contract"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "container",
+              "answers": [
+                    "an object that can be used to hold or store something",
+                    "a large metal box used for transporting goods by road or rail",
+                    "a person who restrains their emotions"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "court",
+              "answers": [
+                    "a body of people presided over by a judge to hear legal cases",
+                    "an area marked out for playing a sport such as tennis",
+                    "a group of people advising a monarch or head of state"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "creature",
+              "answers": [
+                    "an animal or living being, especially a non-human one",
+                    "an unpleasant or ridiculous person",
+                    "something that has been invented by the imagination"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "chips",
+              "answers": [
+                    "long rectangular pieces of potato cooked in deep fat",
+                    "thin crispy slices of potato sold in a bag",
+                    "small pieces of wood or broken material"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "custom",
+              "answers": [
+                    "a traditional and widely accepted way of behaving",
+                    "a set of clothes worn for a particular activity or event",
+                    "a tax paid on goods imported from another country"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "desert",
+              "answers": [
+                    "a waterless, desolate area of land with little vegetation",
+                    "a sweet course eaten at the end of a meal",
+                    "a place where people go to rest and relax"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "diary",
+              "answers": [
+                    "a book in which one keeps a daily record of events",
+                    "a special plan of what to eat in order to stay healthy",
+                    "a collection of official government records"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+         3,
+         102,
+         '{
+              "word": "dress",
+              "answers": [
+                    "a one-piece garment for a woman or girl covering the body and legs",
+                    "a tracksuit or sportswear worn for exercise",
+                    "a formal suit consisting of a jacket and trousers"
+              ],
+              "correct_idx": 0
+         }'
+    ),
+    (
+        3,
+        103,
+        '{
+            "left": [
+                "auteur",
+                "colporteur",
+                "compositor",
+                "courier",
+                "curator",
+                "escort",
+                "machinist",
+                "superintendent",
+                "regisseur",
+                "jongleur"
+            ],
+            "right": [
+                "film, originality, eccentricity",
+                "books, religion, distribution",
+                "printing, books, letters",
+                "tourists, trip, care",
+                "exhibits, museum, library",
+                "company, dinner, event",
+                "train, engine, railway",
+                "building, repairs, supervision",
+                "ballet, choreography, stage",
+                "singing, storytelling, wandering",
+                "factory, machine, operation"
+            ]
+        }'
+    ),
+    (
+        3,
+        103,
+        '{
+            "left": [
+                "adapter",
+                "camera",
+                "conductor",
+                "container",
+                "flagon",
+                "folder",
+                "lorgnette",
+                "parasol",
+                "pulpit",
+                "radiator",
+                "urn",
+                "vase"
+            ],
+            "right": [
+                "for connecting electrical equipment",
+                "for taking pictures",
+                "for letting electricity pass through",
+                "for holding different things or substances",
+                "for holding, serving or selling wine",
+                "for holding loose papers",
+                "for improving one''s sight",
+                "for protecting oneself or others from the sun",
+                "for a priest to stand on and preach from",
+                "for heating rooms",
+                "for holding large amounts of tea or coffee",
+                "for holding cut flowers",
+                "for casting ballots in"
+            ]
+        }'
+    ),
+    (
+        3,
+        103,
+        '{
+            "left": [
+                "adapter",
+                "camera",
+                "conductor",
+                "container",
+                "flagon",
+                "folder",
+                "lorgnette",
+                "parasol",
+                "pulpit",
+                "radiator",
+                "urn",
+                "vase"
+            ],
+            "right": [
+                "for connecting electrical equipment",
+                "for taking pictures",
+                "for letting electricity pass through",
+                "for holding different things or substances",
+                "for holding, serving or selling wine",
+                "for holding loose papers",
+                "for improving one''s sight",
+                "for protecting oneself or others from the sun",
+                "for a priest to stand on and preach from",
+                "for heating rooms",
+                "for holding large amounts of tea or coffee",
+                "for holding cut flowers",
+                "for casting ballots in"
+            ]
+        }'
+    );
